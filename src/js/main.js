65d7ce1de17bb3095f56148e610bbe1450463908
xolms <xolms111@ya.ru>
2017-07-25 17:16:55 +0300

    123

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function($) {
+    // Запуск карусели
+    var owl = $('.slider');
+    owl.owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        nav: false,
+        dots: false
+    });
+    //Открытие меню на мобилках
+    $(".toggle-mnu").click(function() {
+        $(this).toggleClass("on");
+        $(".main-mnu").slideToggle();
+        return false;
+    });
+});
